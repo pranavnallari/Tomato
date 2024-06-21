8f5ff39706503fb7e10c5e30024b14f328f14e1a
Pranav Nallari <61450519+pranavnallari@users.noreply.github.com>
2024-06-21 21:53:27 +0530

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.S)
 // placing user order for frontend
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5174"
+    const frontend_url = "https://tomato-48dp.onrender.com"
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
